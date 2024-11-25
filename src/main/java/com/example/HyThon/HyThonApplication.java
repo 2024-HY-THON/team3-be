4d32b6a2685cddae335780f9678a8db58979edd1
hcg0127 <kej431003@gmail.com>
2024-11-25 17:40:13 +0900

    git init

---
+++ src/main/java/com/example/HyThon/HyThonApplication.java
@@ -0,0 +1,13 @@
+package com.example.HyThon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyThonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyThonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HyThon/HyThonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HyThon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyThonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
