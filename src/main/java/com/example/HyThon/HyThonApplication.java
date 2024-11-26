f55c0d0b2836031fac95f6fc2e668b7f163b2e10
hcg0127 <kej431003@gmail.com>
2024-11-26 14:25:43 +0900

    [FIX] applicaion.yml 수정

--- src/main/java/com/example/HyThon/HyThonApplication.java
+++ src/main/java/com/example/HyThon/HyThonApplication.java
@@ -2,8 +2,14 @@ package com.example.HyThon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableJpaAuditing
+@EnableScheduling
 public class HyThonApplication {
 
 	public static void main(String[] args) {
